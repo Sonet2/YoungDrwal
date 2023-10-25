868c6bbb033b3ca7e8afc9a6ef3ba19efd00fef9
Kegas <53904792+xkegas@users.noreply.github.com>
2023-10-25 11:32:43 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Witamy serdecznie w Sklepie z Drewnem");
+        Zarzadzanie zarzadzanie = new Zarzadzanie();
+        while(true)
+        {
+            zarzadzanie.Menu();
+        }
+    }
+}
